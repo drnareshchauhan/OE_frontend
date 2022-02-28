dd5199b1b634b0a921d1919e4253f775fdff49aa
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-28 18:57:28 +0530

    online ex

--- src/components/basic/header/header.js
+++ src/components/basic/header/header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './header.css'; 
-import main from './main.jpg';
+import React from "react";
+import "./header.css";
+import psm from "./psm.jpg";
 
 function HomepageHeader(props) {
-    return (
-        <div>
-            <div className="header-container-2">
-                <img src={main} alt="company logo" className="logo" />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="header-container-2">
+        <img src={psm} alt="company logo" className="logo" />
+      </div>
+    </div>
+  );
 }
 
 export default HomepageHeader;
